c5c238215dfe0366ef1096169eafefa5d8fe680f
Archie Milligan <agmilligan@wpi.edu>
2022-11-08 19:08:31 -0500

    here ya go

--- src/main.cpp
+++ src/main.cpp
@@ -1,184 +1,125 @@
-// #define __SIMULATE__
-
-#ifdef __SIMULATE__
-
-#include <unistd.h>  // for usleep()
-#include <cstdint>
-#include <iostream>
-
-using std::cout;
-using std::endl;
-
-#define INPUT_PULLUP 2
-
-#define PI (3.14159)
-
-void pinMode(uint8_t, uint8_t) {}
-
-uint8_t digitalRead(uint8_t) { return 0; }
-
-void delay(int t) { }//usleep(t * 1000UL); }
-struct {
-    void begin(int) {}
-    uint8_t read() {
-        return 0;
-    }
-    uint8_t available() {
-        return 0;
-    }
-    template <typename T>
-    void print(T x) {
-        cout << x;
-    }
-    template <typename T>
-    void println(T x) {
-        cout << x << endl;
-    }
-    template <typename T>
-    void printf(T x) {
-        cout << x << endl;
-    }
-    template <typename T, typename D>
-    void printf(T x, D d) {
-        cout << x << d << endl;
-    }
-    template <typename T, typename A, typename B>
-    void printf(T x, A a, B b) {
-        cout << x <<  a << b << endl;
-    }
-} Serial;
-
-int main() {
-    void setup();
-    void loop();
-    setup();
-    for (;;) loop();
-}
-
-#endif
-
 #include <Arduino.h>
-#include <Servo.h>
+#include <Adafruit_PWMServoDriver.h>
 
-#include "comm.h"
-#include "config.h"
-#include "invkine.h"
-#include "touch.h"
+/***************************************************
+  This is an example for our Adafruit 16-channel PWM & Servo driver
+  Servo test - this will drive 8 servos, one after the other on the
+  first 8 pins of the PCA9685
 
-#define __VALIDATE__
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/815
 
-void checkCommands();
+  These drivers use I2C to communicate, 2 pins are required to
+  interface.
 
-char cmdByte = 0;  // for incoming serial data
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
-Servo servo[NUM_LINKS];
-int pin[NUM_LINKS] = {7, 9, 10, 11, 12, 13};
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 
-float dx, dy = 0;
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+// Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+// Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
-// SoftwareSerial touch(0,1);
+// Depending on your servo make, the pulse width min and max may vary, you
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN 150   // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 600   // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN 600      // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX 2400     // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
+
+// our servo # counter
+uint8_t servonum = 0;
 
 void setup() {
-    setupComm();
+    Serial.begin(9600);
+    Serial.println("8 channel Servo test!");
 
-    setupTouch();
+    pwm.begin();
+    /*
+     * In theory the internal oscillator (clock) is 25MHz but it really isn't
+     * that precise. You can 'calibrate' this by tweaking this number until
+     * you get the PWM update frequency you're expecting!
+     * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+     * is used for calculating things like writeMicroseconds()
+     * Analog servos run at ~50 Hz updates, It is importaint to use an
+     * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+     * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
+     *    the I2C PCA9685 chip you are setting the value for.
+     * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+     *    expected value (50Hz for most ESCs)
+     * Setting the value here is specific to each individual I2C PCA9685 chip and
+     * affects the calculations for the PWM update frequency.
+     * Failure to correctly set the int.osc value will cause unexpected PWM results
+     */
+    pwm.setOscillatorFrequency(27000000);
+    pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
-    invkine_setup();
+    delay(10);
+}
 
-    for (int i = 0; i < NUM_LINKS; i++) {  // attach all servos
-        servo[i].attach(pin[i] /*, MIN_BAND, MAX_BAND*/);
-    }
+// You can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
+void setServoPulse(uint8_t n, double pulse) {
+    double pulselength;
+
+    pulselength = 1000000;      // 1,000,000 us per second
+    pulselength /= SERVO_FREQ;  // Analog servos run at ~60 Hz updates
+    Serial.print(pulselength);
+    Serial.println(" us per period");
+    pulselength /= 4096;  // 12 bits of resolution
+    Serial.print(pulselength);
+    Serial.println(" us per bit");
+    pulse *= 1000000;  // convert input seconds to us
+    pulse /= pulselength;
+    Serial.println(pulse);
+    pwm.setPWM(n, 0, pulse);
 }
 
 void loop() {
-    checkCommands();
-
-    checkTouch();
-
-    // static fvector desiredAngles(2);
-    // desiredAngles =
-
-    static fvector servo_d(NUM_LINKS);
-    static fvector last_d(NUM_LINKS);
-
-    // servo_d = invKine(dx, dy);
-    servo_d = invKine(5, -6);
-
-    if (last_d[0] != servo_d[0]) {
-        Serial.printf("Servo Angles:");
-        for (int i = 0; i < NUM_LINKS; i++) {
-            Serial.printf("\t%i: ", i);
-            Serial.print(servo_d[i]);
-        }
-        Serial.println("");
+    // Drive each servo one at a time using setPWM()
+    Serial.println(servonum);
+    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+        pwm.setPWM(servonum, 0, pulselen);
     }
 
-    last_d[0] = servo_d[0];
-
-    for (int i = 0; i < NUM_LINKS; i++) {
-        if (i % 2) {
-            servo[i].write(90 - servo_d[i]);
-        } else {
-            servo[i].write(90 + servo_d[i]);
-        }
+    delay(500);
+    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+        pwm.setPWM(servonum, 0, pulselen);
     }
+
+    delay(500);
+
+    // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
+    // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior.
+    for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
+        pwm.writeMicroseconds(servonum, microsec);
+    }
+
+    delay(500);
+    for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
+        pwm.writeMicroseconds(servonum, microsec);
+    }
+
+    delay(500);
+
+    servonum++;
+    if (servonum > 7) servonum = 0;  // Testing the first 8 servo channels
 }
 
-void checkCommands() {
-    if (Serial.available()) {
-        // read the incoming byte:
-        cmdByte = Serial.read();
-
-        // say what you got:
-        Serial.printf("I received: %d | ", cmdByte);
-
-        servoval_t data;
-
-        switch (cmdByte) {
-            case CMD_WRITE_DEG: {
-                Serial.println("CMD: Write DEG, waiting for data...");
-
-                data = readVal();
-
-                Serial.printf(" RAW 0x%X\n", data.value);
-                servo[data.servo].write(data.value);
-            } break;
-            case CMD_WRITE_US: {
-                Serial.println("CMD: Write us, waiting for data...");
-
-                data = readVal();
-
-                Serial.printf(" %d us\n", data.value);
-                servo[data.servo].writeMicroseconds(data.value);
-            } break;
-            case CMD_WRITE_MDEG: {
-                Serial.println("CMD: Write mDeg, waiting for data...");
-
-                data = readVal();
-
-                float percent = data.value / 180000;
-                uint16_t us = (percent * (2400 - 544)) + 544;
-
-                Serial.printf(" %f degs\n", percent * 180);
-                servo[data.servo].writeMicroseconds(us);
-            } break;
-
-            case CMD_WRITE_PLATFORM_ANGLES: {
-                Serial.print("CMD: Write Angles (integer degrees); X Angle:");
-                uint16_t xIn;
-                uint16_t yIn;
-                xIn = readASCII();
-                Serial.print("Y Angle:");
-                yIn = readASCII();
-
-                dx = xIn;
-                dy = yIn;
-
-                Serial.printf("Writing to angle (%d,%d)\n", xIn, yIn);
-            } break;
-            default:
-                Serial.print(cmdByte);
-                Serial.println("... not a recognized command");
-        }
-    }
+int main() {
+    setup();
+    while(true){
+        loop();
+    } 
+     return 0; 
 }
